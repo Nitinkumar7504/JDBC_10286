3bbfa4775956bac3359e68f5ec1d201791e14d6f
nitin <nitin@LAPTOP-M4B7UJBM>
2024-02-08 23:31:09 +0530

    commit

--- Nitindemo/src/com/nitin/custom/Demo.java
+++
@@ -1,6 +0,0 @@
-package com.nitin.custom;
-public class Demo {
-   public static void main(String args[]) {
-	   System.out.println("nitin");
-   }
-}
---
+++ Nitindemo/src/com/nitin/custom/demo.java
@@ -0,0 +1,6 @@
+package com.nitin.custom;
+public class demo {
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+}
