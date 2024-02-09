b64efb989b1608d32f3d2b3f63b0f748accc8e41
nitin <nitin@LAPTOP-M4B7UJBM>
2024-02-09 11:05:57 +0530

    commit

--- Nitindemo/src/com/nitin/custom/demo.java
+++ Nitindemo/src/com/nitin/custom/demo.java
@@ -1,6 +1,7 @@
 package com.nitin.custom;
 public class demo {
-	public static void main(String[] args) {
-		System.out.println("Hello World");
+	
+	public static void main(String[] args) throws ClassNotFoundException,SQLException {
+		System.out.print("nitin");
 	}
 }
